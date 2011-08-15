dd1004d7b82848f8d1618d4350ebf00523980b63
Lakmal Silva <lakmal_silva@yahoo.com>
2011-08-15 21:49:39 +0200

    Signed-off-by: Lakmal Silva <lakmal_silva@yahoo.com>

---
+++ src/org/totalbeginner/tutorial/Person.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.totalbeginner.tutorial;
+
+/**
+ * @author lakmal
+ *
+ */
+public class Person {
+	//fields
+	private String name; // name of the person
+	private int maximumBooks; // most books the person can check out
+
+	// Constructors
+
+	public Person() {
+		name = "unknown name";
+		maximumBooks = 3; 
+	}
+
+}
